d6b9a7ef281fbe8bcef6c4c552a1262bb5fa45c4
Exercicios_Guanabara <victor.profissional3592@gmail.com>
2025-01-28 10:33:17 -0300

    init project

---
+++ python_básico_para_iniciar_nossos_jogos/metodos.py
@@ -0,0 +1,11 @@
+nome = "Hero"
+vida = 10
+
+gravidade = 9.8
+
+vivo = False
+
+nome = input("Digite o seu nome:")
+vida = input("Digite o seu numero:")
+print(f"nome: ", nome)
+print(f"Vida: ", vida)
